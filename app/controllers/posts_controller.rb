40008878e4ea1ea6617501e5bde8b39b3aff600d
xinyiwang99 <xinyiwang2022@u.northwestern.edu>
2023-03-07 20:58:25 +0000

    17points

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,14 +6,20 @@ class PostsController < ApplicationController
   end
 
   def create
+    if @current_user
     @post = Post.new
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
     @post["user_id"] = @current_user["id"]
+    @post.uploaded_image.attach(params["post"]["uploaded_image"])
     @post.save
     redirect_to "/places/#{@post["place_id"]}"
+    else
+    flash["notice"] = "Login first."
+    end
+
   end
 
 end
